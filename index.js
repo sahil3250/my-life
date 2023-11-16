89a473b73d94e5265c427982ce50209076a171bd
sahil3250 <118569353+sahil3250@users.noreply.github.com>
2023-11-15 22:47:27 -0500

    Add files via upload
    made proper folders

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ const rec = document.querySelectorAll('.rec');
 
 
 gallery.onmousemove = e => {
-    const mouseX = e.clientX,
-          mouseY = e.clientY;
-    
-    const xDecimal = mouseX / window.innerWidth,
-          yDecimal = mouseY / window.innerHeight;
-    
-    const maxX = gallery.offsetWidth - window.innerWidth,
-          maxY = gallery.offsetHeight - window.innerHeight;
-    
-    const panX = maxX * xDecimal * -1,
-          panY = maxY * yDecimal * -1;
+      const mouseX = e.clientX,
+            mouseY = e.clientY;
 
-    gallery.animate({
-      transform: `translate(${panX}px, ${panY}px)`
-    }, {
-      duration: 2000,
-      fill: "forwards",
-      easing: "ease"
-    })
-   
-  
-  }
- 
-  const maxColor = 16777215;
+      const xDecimal = mouseX / window.innerWidth,
+            yDecimal = mouseY / window.innerHeight;
+
+      const maxX = gallery.offsetWidth - window.innerWidth,
+            maxY = gallery.offsetHeight - window.innerHeight;
+
+      const panX = maxX * xDecimal * -1.05,
+            panY = maxY * yDecimal * -1.05;
+
+      gallery.animate({
+            transform: `translate(${panX}px, ${panY}px)`
+      }, {
+            duration: 2000,
+            fill: "forwards",
+            easing: "ease"
+      })
 
 
-  gallery.onmousedown = e =>
-  {
-   
-    rec.forEach((rec) => {
+}
 
-        let color = "#" + Math.floor(Math.random() * maxColor).toString(16);
-      
-          rec.style.backgroundColor = color ;
+const maxColor = 16777215;
 
-        });
 
-       
-  }
+gallery.onmousedown = e => {
+
+      rec.forEach((rec) => {
+
+            let color = "#" + Math.floor(Math.random() * maxColor).toString(16);
+
+            rec.style.backgroundColor = color;
+
+      });
+
+
+
+}
